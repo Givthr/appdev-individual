bc83c9bb247b48c30d8652c26bff6aa0659ce25e
Givthr <vincegiven03@gmail.com>
2026-05-27 21:46:46 +0800

    Add PostgreSQL support

--- mysite/settings.py
+++ mysite/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 load_dotenv()
 
 SECRET_KEY = os.getenv("SECRET_KEY")
-DEBUG = os.getenv("DEBUG") == "True"
+DEBUG = os.getenv("DEBUG", "False") == "True"
 
 env_hosts = os.getenv("ALLOWED_HOSTS")
 if env_hosts:
@@ -87,6 +87,7 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
@@ -103,6 +104,8 @@ STORAGES = {
 SECURE_SSL_REDIRECT = os.getenv("SECURE_SSL_REDIRECT") == "True"
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
+SECURE_BROWSER_XSS_FILTER = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
 
 SECURE_HSTS_SECONDS = 31536000
 SECURE_HSTS_INCLUDE_SUBDOMAINS = True
